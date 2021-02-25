67ed1b8a28d370840aa4af7753a9c89a8174894f
akey500 <77023814+akey500@users.noreply.github.com>
2021-02-25 14:19:52 -0600

    Add files via upload

---
+++ test2.php
@@ -0,0 +1,11 @@
+<?php
+$content = "{'x': 'fish'}";
+$fp = fopen("data.json","r");
+fwrite($fp,$content);
+fclose($fp);
+
+
+$myfile = fopen("data.json", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("data.json"));
+fclose($myfile);
+?>
